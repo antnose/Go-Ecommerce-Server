7645b336fb279b9ddc26cf4381da3b410b326bb4
antnose <antnose28@email.com>
2026-01-27 02:28:24 +0600

    Separate every single middleware

--- server/cmd/serve.go
+++ server/cmd/serve.go
@@ -7,17 +7,22 @@ import (
 )
 
 func Serve() {
-	manager := middleware.NewManager()
 
-	manager.Use()
+	manager := middleware.NewManager()
+	manager.Use(
+		middleware.Preflight,
+		middleware.Cors,
+		middleware.Logger,
+	)
 
 	mux := http.NewServeMux()
+	wrappedMux := manager.WrapMux(mux)
 
 	initRoutes(mux, manager)
 
 	fmt.Println("Server running on port http://localhost:3001")
 
-	err := http.ListenAndServe(":3001", mux)
+	err := http.ListenAndServe(":3001", wrappedMux)
 	if err != nil {
 		fmt.Println("Error starting the server", err)
 	}
