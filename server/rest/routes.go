8ec1e17c23ee7bc742916a5aec6e5c4cb213cefc
antnose <antnose28@email.com>
2026-01-31 16:57:00 +0600

    Make route for login user

--- server/rest/routes.go
+++ server/rest/routes.go
@@ -7,14 +7,11 @@ import (
 )
 
 func initRoutes(mux *http.ServeMux, manager *middleware.Manager) {
-	// mux.Handle("GET /products", middleware.Logger(http.HandlerFunc(handlers.GetProducts)))
 	mux.Handle("GET /products", manager.With(
 		http.HandlerFunc(handlers.GetProducts)))
 
-	// mux.Handle("POST /products", middleware.Logger(http.HandlerFunc(handlers.CreateProduct)))
 	mux.Handle("POST /products", manager.With(http.HandlerFunc(handlers.CreateProduct)))
 
-	// mux.Handle("GET /products/{id}", middleware.Logger(http.HandlerFunc(handlers.GetProductById)))
 	mux.Handle("GET /products/{id}", manager.With(http.HandlerFunc(handlers.GetProduct)))
 
 	mux.Handle("PUT /products/{id}", manager.With(http.HandlerFunc(handlers.UpdateProduct)))
@@ -22,5 +19,7 @@ func initRoutes(mux *http.ServeMux, manager *middleware.Manager) {
 	mux.Handle("DELETE /products/{id}", manager.With(http.HandlerFunc(handlers.DeleteProduct)))
 
 	mux.Handle("POST /users", manager.With(http.HandlerFunc(handlers.CreateUser)))
+
+	mux.Handle("POST /users/login", manager.With(http.HandlerFunc(handlers.Login)))
 
 }
